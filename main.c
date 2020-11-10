304f72b4b572c3b3a0ef83b216ed6b46621f6377
ThbLoth <73190082+ThbLoth@users.noreply.github.com>
2020-11-10 16:37:10 +0100

    Add files via upload
    upload main

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "HUFFMAN.h"
+#include "HUFFMAN.c"
+
+int main(void)
+{
+    FILE* fic = NULL;
+
+    fic = fopen("Alice.txt",r);
+
+    //Insérer les fonctions ci-dessous
+
+    return 0;
+
+}
