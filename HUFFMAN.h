f0d9f27e688912e0bbdb3729c0102643315e5b91
ThbLoth <73190082+ThbLoth@users.noreply.github.com>
2020-12-03 14:03:12 +0100

    Ajout main, .c et .h dans dev_questionA
    ajout de la première question

--- HUFFMAN.h
+++ HUFFMAN.h
@@ -1,5 +1,6 @@
 #ifndef HUFFMAN_H_INCLUDED
 #define HUFFMAN_H_INCLUDED
 
+void Conversion(FILE*);
 
 #endif // HUFFMAN_H_INCLUDED
--- main.c
+++ main.c
@@ -1,16 +1,13 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "HUFFMAN.h"
-#include "HUFFMAN.c"
+#include "huffman.c"
 
-int main(void)
+int main()
 {
-    FILE* fic = NULL;
+    FILE * fic;
+    fic = fopen("Alice.txt","r");
 
-    fic = fopen("Alice.txt",r);
-
-    //Insérer les fonctions ci-dessous
-
+    Conversion(fic);
     return 0;
-
 }
