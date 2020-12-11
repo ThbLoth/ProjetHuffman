02d2b99b309344ab589fcb9b2d89405f3bc20389
ThbLoth <73190082+ThbLoth@users.noreply.github.com>
2020-12-11 16:56:08 +0100

    add creationarbrehuffman.c and creationarbrehuffman.h
    add files for question D

---
+++ CreationArbreHuffman.h
@@ -0,0 +1,9 @@
+#ifndef CREATIONARBREHUFFMAN
+#define CREATIONARBREHUFFMAN
+
+arbre creation_noeud(int, int);
+tree_array* creation_elem_noeud(int, int);
+tree_array* creation_liste_arbre(node*);
+arbre arbre_huffman(tree_array*);
+
+#endif
